5cd19af897df4bcc8713f351d3ce9cf5b874c594
ivanvanya <ivan.bog@yandex.ru>
2019-08-31 10:16:52 +0300

    WIP структуры данных

--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,30 @@
 export const AMOUNT_CARDS_ALL_MOVIES = 5;
 export const AMOUNT_CARDS_TOP_RATED = 2;
 export const AMOUNT_CARDS_MOST_COMMENTED = 2;
+
+
+const getRandomNumberInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const getRandomArrayElement = (array) => array[getRandomNumberInRange(0, array.length - 1)];
+
+const getRandomStringOutOfArray = (array, maxLength) => {
+  const arrayLength = getRandomNumberInRange(1, maxLength);
+  return [...Array(arrayLength)].map(() => getRandomArrayElement(array)).join(` `);
+};
+
+const getContentData = () => {
+  return {
+    description: getRandomStringOutOfArray([
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+      `Cras aliquet varius magna, non porta ligula feugiat eget.`,
+      `Fusce tristique felis at fermentum pharetra.`,
+      `Aliquam id orci ut lectus varius viverra.`,
+      `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
+      `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
+      `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
+      `Sed sed nisi sed augue convallis suscipit in sed felis.`,
+      `Aliquam erat volutpat.`,
+      `Nunc fermentum tortor ac porta dapibus.`,
+      `In rutrum ac purus sit amet tempus.`,
+    ], 3),
+  };
+};
--- src/main.js
+++ src/main.js
@@ -1,11 +1,40 @@
 import createSearchTemplate from './components/seacrh';
 import createProfileTemplate from './components/profile';
 import createMenuTemplate from './components/menu';
-import createCardFilm from './components/card-film';
+import {createCardFilm} from './components/card-film';
 import createButtonShowMore from './components/show-more';
 import createPopupDescFilm from './components/popup';
-import {AMOUNT_CARDS_MOST_COMMENTED, AMOUNT_CARDS_TOP_RATED, AMOUNT_CARDS_ALL_MOVIES} from './data/data';
+import {
+  AMOUNT_CARDS_MOST_COMMENTED,
+  AMOUNT_CARDS_TOP_RATED,
+  AMOUNT_CARDS_ALL_MOVIES} from './data/data';
 
+
+    // type: getRandomArrayElement(Array.from(new Set([...EVENT_TYPES.transfer, ...EVENT_TYPES.activity]))),
+    // city: getRandomArrayElement(CITIES),
+    // price: getRandomNumberInRange(1, 20) * 10, // цена в десятках
+    // description: getRandomStringOutOfArray([
+    //   `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+    //   `Cras aliquet varius magna, non porta ligula feugiat eget.`,
+    //   `Fusce tristique felis at fermentum pharetra.`,
+    //   `Aliquam id orci ut lectus varius viverra.`,
+    //   `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
+    //   `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
+    //   `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
+    //   `Sed sed nisi sed augue convallis suscipit in sed felis.`,
+    //   `Aliquam erat volutpat.`,
+    //   `Nunc fermentum tortor ac porta dapibus.`,
+    //   `In rutrum ac purus sit amet tempus.`,
+    // ], 3),
+    // options: getRandomArrayElements(OPTIONS),
+    // time: getRandomTimeInterval(),
+    // isFavorite: getRandomBoolean(),
+    // photos: [...Array(getRandomNumberInRange(1, 5))].map(() => getRandomNumberInRange(1, 5))
+  };
+};
+
+getContentData();
+console.log(getContentData());
 const siteFilmsList = document.querySelector(`.films-list`);
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = document.querySelector(`.header`);
